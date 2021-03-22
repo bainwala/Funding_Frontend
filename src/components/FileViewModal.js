714c7e147d52d50b0dbf92c4bd0efb3bfe964abb
izanahmed <znahmed148@gmail.com>
2021-03-22 03:28:19 -0500

    Admin more CSS added 3

--- src/components/FileViewModal.js
+++ src/components/FileViewModal.js
@@ -19,12 +19,12 @@ export default function FileViewModal(props) {
           <h5 className="mb-4">Details:</h5>
           <h6 className="my-4"><b>By: </b>{props.info.source}</h6>
           <h6 className="my-4"><b>Name: </b>{props.info.funding_name}</h6>
-          <h6 className="my-4"><b>Description: </b>{props.info.funding_name}</h6>
-          <h6 className="my-4"><b>Amount Provided: </b>{props.info.funding_name}</h6>
-          <h6 className="my-4"><b>Contact Person: </b>{props.info.funding_name}</h6>
-          <h6 className="my-4"><b>Website: </b>{props.info.funding_name}</h6>
-          <h6 className="my-4"><b>Who's eligible? </b>{props.info.funding_name}</h6>
-          <h6 className="mt-4"><b>Deadline: </b>11:59 pm on {props.info.funding_name}</h6>
+          <h6 className="my-4"><b>Description: </b>{props.info.description}</h6>
+          <h6 className="my-4"><b>Amount Provided: </b>{props.info.amount}</h6>
+          <h6 className="my-4"><b>Contact Person: </b>{props.info.contact_person}</h6>
+          <h6 className="my-4"><b>Website: </b>{props.info.web}</h6>
+          <h6 className="my-4"><b>Who's eligible? </b>{props.info.eligible}</h6>
+          <h6 className="mt-4"><b>Deadline: </b>11:59 pm on {props.info.deadline}</h6>
         </Modal.Body>
         <Modal.Footer className="border border-dark">
           <Button 
